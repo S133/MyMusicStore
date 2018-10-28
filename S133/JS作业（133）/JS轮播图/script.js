82fa17b22266d68e3166f77dee94b97b29f0d602
S133 <1339518945@qq.com>
2018-10-28 18:15:59 +0800

    lunbo

---
+++ S133/JS作业（133）/JS轮播图/script.js
@@ -0,0 +1,36 @@
+window.onload=function(){
+    function $(id){
+        return document.getElementById(id);
+    }
+
+
+
+var pic_top=0;
+var timer=null;
+//up onmouseover
+$('picup').onmouseover=function(){
+    //打开计时器 每秒修改top属性
+    clearInterval(timer);
+    timer=setInterval(function(){
+        pic_top--;
+        pic_top>=-1070? $('pic').style.top=pic_top+'px':clearInterval(timer);
+    },20);
+}
+
+
+
+//down onmouseover
+//打开计时器 每秒修改top属性
+$('picdowm').onmouseover=function(){
+    //打开计时器 每秒修改top属性 图片底部时停止
+    clearInterval(timer);
+    timer=setInterval(function(){
+        pic_top++;
+        pic_top<0? $('pic').style.top=pic_top+'px':clearInterval(timer);
+    },20);
+}
+//onmouseout 计时器停
+$("xiaomi").onmouseout=function(){
+    clearInterval(timer);
+}
+}
