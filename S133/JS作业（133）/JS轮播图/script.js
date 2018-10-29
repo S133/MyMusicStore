afd3c6346c4003d15666e7e9931401b55571bc9f
S133 <1339518945@qq.com>
2018-10-29 15:09:36 +0800

    JS小米广告

--- S133/JS作业（133）/JS轮播图/script.js
+++ S133/JS作业（133）/JS轮播图/script.js
@@ -8,29 +8,29 @@ window.onload=function(){
 var pic_top=0;
 var timer=null;
 //up onmouseover
-$('picup').onmouseover=function(){
+$('picUp').onmouseover=function(){
     //打开计时器 每秒修改top属性
     clearInterval(timer);
     timer=setInterval(function(){
         pic_top--;
-        pic_top>=-1070? $('pic').style.top=pic_top+'px':clearInterval(timer);
-    },20);
+        pic_top>=-1070 ? $('pic').style.top=pic_top+'px': clearInterval(timer);
+    }, 20);
 }
 
 
 
 //down onmouseover
 //打开计时器 每秒修改top属性
-$('picdowm').onmouseover=function(){
+$('picDown').onmouseover=function(){
     //打开计时器 每秒修改top属性 图片底部时停止
     clearInterval(timer);
     timer=setInterval(function(){
         pic_top++;
-        pic_top<0? $('pic').style.top=pic_top+'px':clearInterval(timer);
-    },20);
+        pic_top< 0 ? $('pic').style.top = pic_top + 'px': clearInterval(timer);
+    }, 20);
 }
 //onmouseout 计时器停
-$("xiaomi").onmouseout=function(){
+$("xiaomi").onmouseout = function () {
     clearInterval(timer);
 }
 }
