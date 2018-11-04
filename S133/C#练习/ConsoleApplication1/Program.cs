2ac0f17692106514cb5569b926e4e3e1414aa838
S133 <1339518945@qq.com>
2018-11-04 11:03:36 +0800

    w

--- S133/C#练习/ConsoleApplication1/Program.cs
+++ S133/C#练习/ConsoleApplication1/Program.cs
@@ -18,17 +18,22 @@ namespace Demo
                 foreach (var d in departments)
                     Console.WriteLine("编号{0},部门名称{1},说明{2}", d.SortCode, d.Name, d.Dscn);
 
-                Console.WriteLine("修改记录");
+                //Console.WriteLine("修改记录");
 
-                var editDepartment = context.Departments.SingleOrDefault(x => x.Name == "环境与食品学院");
-                if (editDepartment != null)
-                {
-                    editDepartment.Name = "环境与食品检测学院";
-                    editDepartment.SortCode = "007";
-                    context.SaveChanges();
-                }
-                else
-                    Console.WriteLine("未找到该记录，不能修改");
+                //var editDepartment = context.Departments.SingleOrDefault(x => x.Name == "环境与食品学院");
+                //if (editDepartment != null)
+                //{
+                //    editDepartment.Name = "环境与食品检测学院";
+                //    editDepartment.SortCode = "007";
+                //    context.SaveChanges();
+                //}
+                //else
+                //    Console.WriteLine("未找到该记录，不能修改");
+
+                //Console.WriteLine("删除记录");
+                //Console.WriteLine("====================================");
+                //var deIDept = context.Departments.SingleOrDefault(x => x.ID == id);
+                
                 var departments1 = context.Departments.OrderBy(n => n.SortCode).ToList();
                 foreach (var d in departments)
                     Console.WriteLine("编号{0},部门名称{1},说明{2}", d.SortCode, d.Name, d.Dscn);
