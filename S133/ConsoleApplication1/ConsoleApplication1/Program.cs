ccb911033dc5c91570633ec1bd4c952e46be3d85
S133 <1339518945@qq.com>
2018-11-04 09:03:49 +0800

    读取

--- S133/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ S133/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,16 +1,24 @@
-﻿using System;
+﻿using ConsoleApplication1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication1
+namespace Demo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("我日");
+            using (var context = new CourseContext())
+            {
+                var departments = context.Departments.Where(n => n.Name.Contains("工程")).OrderBy(n => n.SortCode).ToList();
+
+                foreach (var d in departments)
+                    Console.WriteLine("编号{0},部门名称{1},说明{2}", d.SortCode, d.Name, d.Dscn);
+                Console.ReadKey();
+            }
         }
     }
 }
