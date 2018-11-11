1fda390d19a104d0b3857f6fd9e5f0540e07d76c
S133 <1339518945@qq.com>
2018-11-11 15:31:44 +0800

    网页

---
+++ S133/web/ASPNETWebFormDemo/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASPNETWebFormDemo.Startup))]
+namespace ASPNETWebFormDemo
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
