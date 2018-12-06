d9bdc84ee7409c59161ea8e82d454a23bff5f955
S133 <1339518945@qq.com>
2018-12-06 08:52:54 +0800

    完成注册

--- S133/5MusicStore1.1/MusicSrore101/MusicStore/Controllers/AccountController.cs
+++ S133/5MusicStore1.1/MusicSrore101/MusicStore/Controllers/AccountController.cs
@@ -22,6 +22,40 @@ namespace MusicStore.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Register(RegisterViewModel model)
         {
+            if (ModelState.IsValid)
+            {
+                var person = new Person()
+                {
+                    FirstName = model.FullName.Substring(0, 1),
+                    LastName = model.FullName.Substring(1, model.FullName.Length - 1),
+                    Name = model.FullName,
+                    CredentialsCode = "",
+                    Birthday = DateTime.Now,
+                    Sex = true,
+                    MobileNumber = "13785642563",
+                    Email = model.Email,
+                    TelephoneNumber = "13785642563",
+                    Description = "",
+                    CreateDateTime = DateTime.Now,
+                    UpdateTime = DateTime.Now,
+                    InquiryPassword = "未设置",
+                };
+                var user = new ApplicationUser()
+                {
+                    UserName = model.UserName,
+                    FirstName = model.FullName.Substring(0, 1),
+                    LastName = model.FullName.Substring(1, model.FullName.Length - 1),
+                    ChineseFullName = model.FullName,
+                    MobileNumber = "13785642563",
+                    Email = model.Email,
+                    Person = person,
+                };
+                var idManager = new IdentityManager();
+                idManager.CreateUser(user, model.PassWord);
+                idManager.AddUserToRole(user.Id, "RegisterUser");
+
+                return Content("<script>alert('恭喜注册成功!');location.href='" + Url.Action("login", "Account") + "'</script>");
+            }
             return View();
         }
         public ActionResult Login(string returnUrl = null)
