50b7410d86c0a64e33e7f3562f506b97d972d1a3
S133 <1339518945@qq.com>
2018-12-09 09:59:39 +0800

    购物车浏览与删除

--- S133/5MusicStore1.1/MusicSrore101/MusicStore/Controllers/ShoppingCartController.cs
+++ S133/5MusicStore1.1/MusicSrore101/MusicStore/Controllers/ShoppingCartController.cs
@@ -46,5 +46,53 @@ namespace MusicStore.Controllers
             }
             return Json(message);
         }
+        public ActionResult Index()
+        {
+            //判断用户是否登录
+            if (Session["LoginUserSessionModel"] == null)
+                return RedirectToAction("login", "Account", new { returnUrl = Url.Action("index", "ShoppingCart") });
+            //查询出当前登录用户
+            var person = (Session["LoginUserSessionModel"] as LoginUserSessionModel).Person;
+            //查询出该用户的购物车
+            var carts = _context.Carts.Where(x => x.Person.ID == x.Person.ID).ToList();
+            //算出购物车的总价
+            decimal? totalprice = (from item in carts select item.Count * item.Album.Price).Sum();
+            //创建视图模型
+            var cartVM = new ShoppingCartViewModel()
+            {
+                CartItems = carts,
+                CartTotalPrice = totalprice ?? decimal.Zero
+            };
+
+            return View(cartVM);
+        }
+
+        [HttpPost]
+        public ActionResult RemoveCart(Guid id)
+        {
+            if (Session["LoginUserSessionModel"] == null)
+                return RedirectToAction("login", "Account", new { returnUrl = Url.Action("index", "ShoppingCart") });
+            var person = (Session["LoginUserSessionModel"] as LoginUserSessionModel).Person;
+            var cartitem = _context.Carts.Find(id);
+            if (cartitem.Count > 1)
+                cartitem.Count--;
+            else
+                _context.Carts.Remove(cartitem);
+            _context.SaveChanges();
+
+            var carts = _context.Carts.Where(x => x.Person.ID == person.ID).ToList();
+            var totalPrice = (from item in carts select item.Count * item.Album.Price).Sum();
+            var htmlString = "";
+            foreach(var item in carts)
+            {
+                htmlString += "<tr>";
+                htmlString += "<td><a href='../store/detail" + item.ID + "'>" + item.Album.Title + "</a></td>";
+                htmlString += "<td>" + item.Album.Price.ToString("C") + "</td>";
+                htmlString += "<td>" + item.Count + "</td>";
+                htmlString += "<td><a href=\"#\"onclick=\"removeCart('" + item.ID + "');\"><i class=\"glyphicon  glyphicon-remove\"></i>移除购物车</a></td><tr>";
+            }
+            htmlString += "<tr><td></td><td></td><td>总价</td><td>" + totalPrice.ToString("C") + "</td></tr>";
+            return Json(htmlString);
+        }
     }
 }
